f74550ca7c4adc7f38541305f6d045c11859df63
Merve Salman <mervesalman_07@hotmail.com>
2025-10-22 16:12:32 +0300

    comment lines added

--- TaskManagerAPI/Data/AppDbContext.cs
+++ TaskManagerAPI/Data/AppDbContext.cs
@@ -3,10 +3,15 @@ using TaskManagerAPI.Models;
 
 namespace TaskManagerAPI.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : DbContext //appDbContext sınıfını oluşturuyoruz. DbContextten kalıtım alıyoruz.
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
+        //constructor method. 
+        // EF Core’a veritabanı ayarlarını (örneğin InMemory veya SQL Server) iletir.
+        // Bu constructor, Dependency Injection ile otomatik olarak çağrılır.
+        
         public DbSet<TaskItem> Tasks => Set<TaskItem>();
+        // Veritabanındaki "Tasks" tablosunu temsil eder.
+        // TaskItem modeli üzerinden CRUD işlemleri yapılmasını sağlar.
     }
 }
